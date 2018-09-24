009db28da2cca878e685c1bcc7b4b630a9404c8f
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-24 17:55:55 +0300

    bp_tomograph

--- experiment-mock/__init__.py
+++ experiment-mock/__init__.py
@@ -7,5 +7,6 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.register_blueprint(routes.bp_main)
+    app.register_blueprint(routes.bp_tomograph)
 
     return app
--- experiment-mock/routes.py
+++ experiment-mock/routes.py
@@ -2,9 +2,14 @@ from flask import Blueprint
 
 
 bp_main = Blueprint('main', __name__, url_prefix='/')
+bp_tomograph = Blueprint('tomograph', __name__, url_prefix='/tomograph')
 
 
 @bp_main.route('/', methods=['GET'])
 def main_route():
     return 'Experiment mock'
 
+
+@bp_tomograph.route('/', methods=['GET'])
+def tomograph():
+    return 'TOMOGRAPH'
