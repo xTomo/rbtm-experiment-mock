e9d826c6f6621e355cebd90fef8e8ccbcfed4e86
gogbajbobo <grigoriev.maxim@gmail.com>
2018-09-25 08:18:16 +0300

    comment

--- experiment-mock/routes.py
+++ experiment-mock/routes.py
@@ -5,6 +5,7 @@ bp_main = Blueprint('main', __name__, url_prefix='/')
 bp_tomograph = Blueprint('tomograph', __name__, url_prefix='/tomograph/<int:tomo_num>')
 
 
+# Base routes
 @bp_main.route('/', methods=['GET'])
 def main_route():
     return 'RBTM experiment mock'
